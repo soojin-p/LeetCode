f1739a412fd91238d15e3547d23438b357c23a9e
soojin-p <72116811+soojin-p@users.noreply.github.com>
2022-01-09 18:58:36 +0900

    Time: 7 ms (39.53%), Space: 12.6 MB (62.04%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -13,36 +13,31 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
        
-        vector<vector<int>> res;
-        //Node를 담을 큐
-        queue<TreeNode*> q_node;
-        if (root == NULL) {
-            return {};
-        }
-        //root가 null이 아니라면 queue에 넣습니다.
-        q_node.push(root);
-        while (!q_node.empty()) {
-            vector<int> level_nodes;
-            int size = q_node.size();
-            for (int i = 0; i < size; i++) {
-                //tmp는 임시 노드입니다.
-                TreeNode* tmp = q_node.front();
-                //임시 노드를 벡터에 넣습니다.
-                level_nodes.push_back(tmp->val);
-                //left가 비어있지 않으면 큐에 넣고
-                if (tmp->left != NULL) {
-                    q_node.push(tmp->left);
-                }
-                //right가 비어있지 않으면 마찬가지로 큐에 넣습니다.
-                if (tmp->right != NULL) {
-                    q_node.push(tmp->right);
-                }
-                // Don't forget to pop q_node INSIDE the for loop after visit it.
-                q_node.pop();
-            }
+    vector<vector<int>> ans;
+        
+    queue< TreeNode*> q;
+    q.push(root);
+    if(!root)
+        return {};
+
+    while (!q.empty())
+    {  
+        vector<int>node;
+        int size = q.size();
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode* tmp = q.front();
+             node.push_back(tmp->val);
+            if (tmp->left!=NULL) q.push(tmp->left);
+            if (tmp->right!=NULL) q.push(tmp->right);
+            
+           // cout<<q.front()->val<<" ";
            
-            res.push_back(level_nodes);
+            q.pop();
+            
         }
-        return res;
+        ans.push_back(node);
+    }
+    return ans;
     }
 };
