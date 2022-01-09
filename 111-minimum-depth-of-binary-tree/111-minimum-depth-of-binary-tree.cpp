d71c5dacc3d8a763d4c524384d10d756433fc067
soojin-p <72116811+soojin-p@users.noreply.github.com>
2022-01-09 19:35:49 +0900

    Time: 552 ms (5.08%), Space: 144.8 MB (43.50%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,12 +12,14 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-     if(root==NULL) return 0;
-        int lh=minDepth(root->left);
-        int rh=minDepth(root->right);
-        //cout<<lh<<" "<<rh<<endl;
-        if(lh==0) return 1+rh; //lh=0 means there is no left subtree,so we cannot compare its height to right subtree
-        else if(rh==0) return 1+lh; //rh=0 means there is no right subtree
-        else return 1+ min(lh,rh);
+        if (!root) return 0;
+    
+        if (!root->left && !root->right) return 1;
+
+        if(!root->left) return minDepth(root->right)+1;
+
+        if(!root->right) return minDepth(root->left)+1;
+
+        return min(minDepth(root->left)+1, minDepth(root->right)+1);
     }
 };
