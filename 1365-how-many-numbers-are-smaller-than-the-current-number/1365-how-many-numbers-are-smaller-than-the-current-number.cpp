29f42e8e93401816a5ccbd301f6a207eddcb1bb0
soojin-p <72116811+soojin-p@users.noreply.github.com>
2022-01-18 22:23:29 +0900

    Time: 12 ms (67.85%), Space: 10.1 MB (68.19%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int conter(vector<int>& nums, int key)
-{
-	int cnt = 0;
-	for (int i = 0; i < nums.size(); i++) {
-		if (key > nums[i])
-			cnt++;
-	}
-	return cnt;
-}
-
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-   vector<int>ans;
-	
-	for (int i = 0; i < nums.size(); i++){
-		ans.push_back(conter(nums, nums[i]));
+        int hash[101] = { 0 }, count = 0;
+																		 // 0 1 2 3 4 5 6 7 8 
+	for (auto num : nums) hash[num]++; // 0 1 2 1 0 0 0 0 1
+	for (int i = 0; i < 101; i++) {
+		if (hash[i] != 0) {//앞에 있는 정수를 더해서 hash[i]에 입력
+			int t = hash[i];	 //t=1
+			hash[i] = count; 
+			count += t; //cnt=1+2 -> 3보다 작은 값의 갯수 
+		}
 	}
+	vector<int> ans; //4 0 1 1 3
+	for (auto num : nums) ans.push_back(hash[num]);
 	return ans;
     }
 };
