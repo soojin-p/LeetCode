d34ba543899fe9c87ea237d2035e15fe728f4f26
soojin-p <72116811+soojin-p@users.noreply.github.com>
2024-07-11 01:11:06 +0900

    Time: 9 ms (52.68%), Space: 45 MB (21.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[][] numsIndex = new int[nums.length][2];
+
+        for (int i = 0; i < nums.length; i++) {
+            numsIndex[i][0] = nums[i];
+            numsIndex[i][1] = i;
+        }
+        Arrays.sort(numsIndex, Comparator.comparingInt(a -> a[0]));
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = numsIndex[l][0] + numsIndex[r][0];
+
+            if (sum == target) {
+                return new int[] { numsIndex[l][1], numsIndex[r][1] };
+            }
+            if (sum > target)
+                r--;
+            if (sum < target)
+                l++;
+        }
+        return null;
+    }
+}
