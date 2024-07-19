3778c906bdfd4dc09be89d2dc654fbb5d281cb7b
soojin-p <72116811+soojin-p@users.noreply.github.com>
2024-07-19 17:53:54 +0900

    Time: 17 ms (54.99%), Space: 45.2 MB (80.08%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,9 +1,9 @@
 class Solution {
-    private int row;
-    private int col;
-    private boolean[][] visited;
-    private final int[] dr = { -1, -1, -1, 0, 1, 1, 1, 0 };
-    private final int[] dc = { -1, 0, 1, 1, 1, 0, -1, -1 };
+    static int row;
+    static int col;
+    static boolean[][] visited;
+    static int[] dr = { -1, -1, -1, 0, 1, 1, 1, 0 };
+    static int[] dc = { -1, 0, 1, 1, 1, 0, -1, -1 };
 
     private boolean isValid(int[][] grid, int r, int c) {
         return (r >= 0 && r < row) && (c >= 0 && c < col) && (grid[r][c] == 0) && !visited[r][c];
@@ -15,6 +15,7 @@ class Solution {
 
         visited = new boolean[row][col];
         
+        //시작 셀이 1이거나 도착셀이 1이면 return -1;
         if (grid[0][0] == 1 || grid[row - 1][col - 1] == 1) {
             return -1;
         }
@@ -26,8 +27,9 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] { r, c });
         visited[r][c] = true;
-        int pathLength = 1;
+        int pathCount =1;
 
+        // bfs 
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
@@ -35,8 +37,9 @@ class Solution {
                 int curRow = cur[0];
                 int curCol = cur[1];
 
+                // 도착 셀에 도착하면, pathCount return;
                 if (curRow == row - 1 && curCol == col - 1) {
-                    return pathLength;
+                    return pathCount;
                 }
 
                 for (int j = 0; j < 8; j++) {
@@ -49,7 +52,7 @@ class Solution {
                     }
                 }
             }
-            pathLength++;
+            pathCount++;
         }
         return -1;
     }
