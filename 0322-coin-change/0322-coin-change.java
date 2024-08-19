0eb25c2e9220387efa6a030909d93a4099fb3015
soojin-p <72116811+soojin-p@users.noreply.github.com>
2024-08-19 16:17:50 +0900

    Time: 14 ms (88.74%), Space: 44.3 MB (58.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,48 +1,24 @@
-import java.util.*;
-
 class Solution {
-    /**
-     * 접근 방법:
-     * 주어진 동전을 사용하여 특정 금액(amount)을 만드는 데 필요한 최소 동전 개수를 찾는 문제.
-     * 각 동전의 사용 횟수는 무제한이므로, 모든 가능한 조합을 탐색.
-     * 
-     * - BFS를 사용 이유:
-     *   1. 각 단계에서 동전을 추가할 때마다 가능한 모든 금액을 탐색하므로, 
-     *      가장 먼저 목표 금액을 달성하는 경로가 최소 동전 개수.
-     *   2. BFS는 최단 경로 탐색에 적합-> 동전의 조합으로 금액을 만드는 데 필요한 최소 동전 수 탐색.
-     */
     public int coinChange(int[] coins, int amount) {
 
-        if (amount == 0)
-            return 0;
+        int[] dp = new int[amount + 1];
+        int max = amount + 1;
 
-        Queue<Entry> queue = new ArrayDeque<>();
-        boolean[] visit = new boolean[amount + 1];
-        queue.add(new Entry(amount, 0));
-
-        while (!queue.isEmpty()) {
-            Entry cur = queue.remove();
-
-            for (int i = 0; i < coins.length; i++) {
-                int nextAmount = cur.amount - coins[i];
-                if (nextAmount == 0) 
-                    return cur.count + 1;
-                else if (nextAmount > 0 && !visit[nextAmount]) {
-                    queue.add(new Entry(nextAmount, cur.count + 1));
-                    visit[nextAmount] = true;
+        for (int i = 0; i <= amount; i++) {
+            dp[i] = max;
+        }
+        dp[0] = 0;
+        // DP 
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    
                 }
             }
         }
-        return -1;
-    }
-
-    static class Entry {
-        int amount;
-        int count;
-
-        public Entry(int amount, int count) {
-            this.amount = amount;
-            this.count = count;
-        }
+       
+        // dp[amount]가 초기화된 값이면 -1 반환
+        return dp[amount] == max ? -1 : dp[amount];
     }
 }
